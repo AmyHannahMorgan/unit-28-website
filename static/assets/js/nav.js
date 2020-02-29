8f52e8a10182c9abcb9ab824d1a46e1a627789de
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-02-29 10:38:12 +0000

    + nav open and close - console logs

--- static/assets/js/nav.js
+++ static/assets/js/nav.js
@@ -1,14 +1,16 @@
+const nav = document.querySelector('nav');
 const navItems = document.querySelectorAll('.navItem');
+const navOpen = document.querySelector('#mobileNavButton');
+const navClose = nav.querySelector('.navClose');
 
 for(let i = 0; i < navItems.length; i++) {
     navItems[i].querySelector('.navHeader').addEventListener('click', e => {
-        console.log(e);
         let parent = e.target.parentNode;
 
         while(!parent.classList.contains('navItem')) {
             parent = parent.parentNode;
         }
-        console.log(parent.querySelector('.navBody'));
+        
         parent.classList.toggle('active');
         if(parent.querySelector('.navBody').style.height == '') {
             parent.querySelector('.navBody').style.height = `${parent.querySelector('.navBody').scrollHeight}px`;
@@ -17,4 +19,11 @@ for(let i = 0; i < navItems.length; i++) {
             parent.querySelector('.navBody').style.height = '';
         }
     }, true);
+}
+
+navOpen.addEventListener('click', toggleNav);
+navClose.addEventListener('click', toggleNav);
+
+function toggleNav() {
+    nav.classList.toggle('active');
 }
