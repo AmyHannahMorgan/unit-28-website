3ef8625d509f08965e9798536497f97691541481
Glitch (amyhannahmorgan) <none>
2020-03-13 09:41:41 +0000

    Import of AmyHannahMorgan/unit-28-website

--- index.js
+++ index.js
@@ -1,7 +1,94 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const githubApiAddress = 'https://api.github.com';
+const fs = require('fs');
+const dataString = './data.json';
+let data = require(dataString);
 const app = express();
 const port = process.env.PORT || 3000;
+
+if(Date.now() >= data.githubProjects.lastUpdated + data.githubProjects.refreshInterval) {
+    updateProjects(data.githubProjects.projects, data.githubProjects.refreshInterval, data);
+}
+
+app.get('/api/projects', (req, res) => {
+    res.send(data.githubProjects);
+});
+
+app.get('/api/projects/display', (req, res) => {
+    let resObj = {
+        projectNames: []
+    }
+    let end = data.githubProjects.projects.length > 4 ? 5 : data.githubProjects.projects.length;
+    for(let i = 0; i < end; i++) {
+        resObj.projectNames.push(data.githubProjects.projects[i].name);
+    }
+    res.send(resObj);
+})
 
 app.use(express.static(`${__dirname}/static`));
 
 app.listen(port);
+
+async function updateProjects(projectsArray, refreshInterval, dataObject) {
+    let array = [];
+    let projects = await axios.get(`${githubApiAddress}/users/${dataObject.githubProjects.username}/repos`, {
+        headers: {
+            'Accept': 'application/vnd.github.v3+json'
+        },
+        params: {
+            sort: 'created'
+        },
+        auth: {
+            username: process.env.GH_CLIENT_ID,
+            password: process.env.GH_CLIENT_SECRET
+        }
+    });
+    for(let i = 0; i < projects.data.length; i++) {
+        let obj = {
+            name: '',
+            description: ''
+        }
+        let descriptionPath = projects.data[i].contents_url.replace('{+path}', 'description.html');
+        let test;
+
+        try {
+            test = await axios.get(descriptionPath, {
+                headers: {
+                    'Accept': 'application/vnd.github.v3+json'
+                },
+                auth: {
+                    username: process.env.GH_CLIENT_ID,
+                    password: process.env.GH_CLIENT_SECRET
+                }
+            });
+            console.log(test);
+            if(test.status === 200) {
+                obj.name = projects.data[i].name;
+                obj.description = test.data.url;
+                console.log(obj);
+                array.push(obj);
+            } 
+        } catch (error) {
+            // console.log(error);
+        }
+    };
+    console.log({array});
+    dataObject.githubProjects.projects = [...array];
+    dataObject.githubProjects.lastUpdated = Date.now();
+
+    console.log(dataObject);
+    updateData(dataObject);
+    setTimeout(() => {
+        updateProjects(projectsArray, refreshInterval, dataObject);
+    }, refreshInterval);
+}
+
+function updateData(newDataObject) {
+    fs.writeFile(dataString, JSON.stringify(newDataObject), (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+}
--- static/assets/js/nav.js
+++ static/assets/js/nav.js
@@ -1,7 +1,10 @@
+const apiAddress = `${window.location.origin}/api`;
 const nav = document.querySelector('nav');
 const navItems = document.querySelectorAll('.navItem.dropdown');
 const navOpen = document.querySelector('#mobileNavButton');
 const navClose = nav.querySelector('.navClose');
+
+const projectsElem = document.querySelector('#projects');
 
 for(let i = 0; i < navItems.length; i++) {
     navItems[i].querySelector('.navHeader').addEventListener('click', e => {
@@ -23,6 +26,26 @@ for(let i = 0; i < navItems.length; i++) {
 
 navOpen.addEventListener('click', toggleNav);
 navClose.addEventListener('click', toggleNav);
+
+let projectsReq = new XMLHttpRequest();
+
+projectsReq.open('GET', `${apiAddress}/projects/display`);
+projectsReq.addEventListener('load', (e) => {
+    let obj = JSON.parse(e.target.response);
+    for(let i = 0; i < obj.projectNames.length; i++) {
+        let li = document.createElement('li');
+        let h1 = document.createElement('h1');
+        let a = document.createElement('a');
+
+        a.innerText = obj.projectNames[i];
+        a.href = `/projects?project=${obj.projectNames[i]}`;
+
+        h1.appendChild(a);
+        li.appendChild(h1);
+        projectsElem.appendChild(li);
+    }
+});
+projectsReq.send();
 
 function toggleNav() {
     nav.classList.toggle('active');
