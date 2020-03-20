e3701107333953cff0e398c857f3ae0c9df03991
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-03-20 14:53:42 +0000

    ~accounted for non technical qualifications

--- static/assets/js/cv.js
+++ static/assets/js/cv.js
@@ -13,9 +13,14 @@ if(cvElem !== null) {
         let response = JSON.parse(e.target.response);
         buildHeader(response.details);
         buildExperience(response.experience);
-        buildQualifications(response.qualifications);
-        if(params.get('cvType') !== null && params.get('cvType') === 'technical') buildSkills(response.skills.technical);
-        else buildSkills(response.skills.nontechnical);
+        if(params.get('cvType') !== null && params.get('cvType') === 'technical') {
+            buildQualifications(response.qualifications, true);
+            buildSkills(response.skills.technical);
+        }
+        else {
+            buildQualifications(response.qualifications, false);
+            buildSkills(response.skills.nontechnical);
+        }
         buildAchievements(response.achievements);
     });
     request.send();
@@ -32,47 +37,56 @@ else if(achievementsElem !== null) {
 
 }
 
-function buildQualifications(qualArray) {
+function buildQualifications(qualArray, technical) {
     for(let i = 0; i < qualArray.length; i++) {
+        if(technical && !qualArray[i].technical) {
+            continue;
+        }
         let qualHolder = document.createElement('div');
         qualHolder.classList.add('qualification');
 
-        let qualTitle = document.createElement('h2');
+        let qualTitle = document.createElement(technical ? 'h2' : 'p');
         qualTitle.classList.add('titleAndGrade');
         qualTitle.innerText = `${qualArray[i].qualification} - ${qualArray[i].grade}`;
-
+    
         let infoHolder = document.createElement('div');
-        infoHolder.classList.add('infoHolder');
-
         let qualLocation = document.createElement('p');
-        qualLocation.classList.add('location');
-        qualLocation.innerText = `${qualArray[i].location}`;
-
         let divider = document.createElement('p');
-        divider.innerText = ' | '
-
         let qualDates = document.createElement('p');
-        qualDates.classList.add('dates');
 
-        let startDate = new Date(qualArray[i].startDate.year, qualArray[i].startDate.month, qualArray[i].startDate.day);
-        let startDateString = startDate.toDateString().split(' ');
-        startDateString = [startDateString[1], startDateString[3]].join(' ');
+        if(technical) {
+            infoHolder.classList.add('infoHolder');
 
-        let endDate = qualArray[i].endDate === null ? 'present' : new Date(qualArray[i].endDate.year, qualArray[i].endDate.month, qualArray[i].endDate.day);
-        let endDateString;
-        if(endDate !== 'present') {
-            endDateString = endDate.toDateString().split(' ');
-            endDateString = [endDateString[1], endDateString[3]].join(' ');
+            qualLocation.classList.add('location');
+            qualLocation.innerText = `${qualArray[i].location}`;
+
+            divider.innerText = ' | '
+
+            qualDates.classList.add('dates');
+
+            let startDate = new Date(qualArray[i].startDate.year, qualArray[i].startDate.month, qualArray[i].startDate.day);
+            let startDateString = startDate.toDateString().split(' ');
+            startDateString = [startDateString[1], startDateString[3]].join(' ');
+
+            let endDate = qualArray[i].endDate === null ? 'present' : new Date(qualArray[i].endDate.year, qualArray[i].endDate.month, qualArray[i].endDate.day);
+            let endDateString;
+            if(endDate !== 'present') {
+                endDateString = endDate.toDateString().split(' ');
+                endDateString = [endDateString[1], endDateString[3]].join(' ');
+            }
+            else endDateString = endDate
+
+            qualDates.innerText = `${startDateString} - ${endDateString}`;
         }
-        else endDateString = endDate
-
-        qualDates.innerText = `${startDateString} - ${endDateString}`;
-
+        
         qualHolder.appendChild(qualTitle);
-        infoHolder.appendChild(qualLocation);
-        infoHolder.appendChild(divider);
-        infoHolder.appendChild(qualDates);
-        qualHolder.appendChild(infoHolder);
+        if(technical) {
+            infoHolder.appendChild(qualLocation);
+            infoHolder.appendChild(divider);
+            infoHolder.appendChild(qualDates);
+            qualHolder.appendChild(infoHolder);
+        }
+        else qualificationsElem.classList.add('nontechnical');
         qualificationsElem.appendChild(qualHolder);
     }
 }
